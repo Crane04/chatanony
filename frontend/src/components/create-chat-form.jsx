c15e21a401b4b9cdab9f8749958a9be0ea44c1bf
Mayowa <mayowayusuf04@gmail.com>
2023-12-18 17:33:42 +0100

    update requirements and fix bugs

--- frontend/src/components/create-chat-form.jsx
+++ frontend/src/components/create-chat-form.jsx
@@ -6,7 +6,13 @@ const CreateChatForm = ({createNewChat, group_, setNewGroup, isLoading, error}) 
         <form method="post" onSubmit={createNewChat}>
             <label htmlFor="groupname">Name:</label>
             <input type="text" value = {group_} onChange={(e) => {setNewGroup(e.target.value)}} id="groupname" name="groupname" placeholder="E.g Hangout with Crane"  required/>
-            <button disabled = {isLoading} >Create Group</button>
+            {
+              isLoading ? 
+              <button disabled = {isLoading} style={{background:"grey"}}>Create Group</button>
+              :
+              <button disabled = {isLoading} >Create Group</button>
+            }
+            
         </form>
         {error ? <div>An error occured, please try again!</div>:  ""}
                 
