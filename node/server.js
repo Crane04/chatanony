68aa2bb484473866453112cdcfc36e455f17fab9
Yusuf Saubana Mayowa <101975553+Crane04@users.noreply.github.com>
2024-03-27 17:42:36 +0100

    Update server.js

--- node/server.js
+++ node/server.js
@@ -2,41 +2,31 @@ const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
 
-// Mapping to store WebSocket connections for each chat room
-const chatRooms = {};
+const rooms = {};
 
-app.ws('/chat/:id', function(ws, req) {
-    const { id } = req.params;
-    console.log('New WebSocket connection for chat room:', id);
-    
-    // Store the WebSocket connection for the corresponding chat room
-    if (!chatRooms[id]) {
-        chatRooms[id] = [];
+app.ws('/chat/:id', (ws, req) => {
+    const roomId = req.params.id;
+
+    // Join the room
+    if (!rooms[roomId]) {
+        rooms[roomId] = [];
     }
-    chatRooms[id].push(ws);
+    rooms[roomId].push(ws);
 
-    ws.on('message', function(msg) {
-        console.log('Received message:', msg);
-
-        // Broadcast the received message to all clients in the same chat room
-        if (chatRooms[id]) {
-            chatRooms[id].forEach(function(client) {
-                if (client.readyState === 1) { // Check if the client is open
-                    client.send(msg);
-                }
-            });
-        }
+    ws.on('message', (msg) => {
+        // Broadcast message to all clients in the same room
+        rooms[roomId].forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(msg);
+            }
+        });
     });
 
-    // Cleanup WebSocket connection when it closes
-    ws.on('close', function() {
-        if (chatRooms[id]) {
-            const index = chatRooms[id].indexOf(ws);
-            if (index > -1) {
-                chatRooms[id].splice(index, 1);
-            }
-        }
+    ws.on('close', () => {
+        // Remove client from the room
+        rooms[roomId] = rooms[roomId].filter(client => client !== ws);
     });
 });
+
 
 app.listen(3000);
